7554f93c902be8176115be5443f7fe311ba2dea0
odiaivie1 <ivieodia0@gmail.com>
2023-08-14 17:53:51 -0700

    Task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
+
+int_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for y in range(len(matrix[i])):
             print("{:d}".format(matrix[i][y]), end="")
             if y != (len(matrix[u]) - 1):
-                rint(" ", end="")
+                print(" ", end="")
 
                 print("")
