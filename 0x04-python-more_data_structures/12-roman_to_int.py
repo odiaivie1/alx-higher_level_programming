d7e109b5b470c263c3df5d04efcda612d3e70a13
odiaivie1 <ivieodia0@gmail.com>
2023-08-16 13:49:32 -0700

    Task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -16,7 +16,7 @@ def to_subtract(list_num):
             if not isinstance(roman_string, str):
                 return 0
 
-            rom_n = {'l': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+            rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
             list_keys = list(rom_n.keys())
 
             num = 0
@@ -27,13 +27,13 @@ def to_subtract(list_num):
                 for r_num in list_keys:
                     if r_num == ch:
                         if rom_n.get(ch) <= last_rom:
-                            num+= to_subtract(list_num)
+                            num += to_subtract(list_num)
                             list_num = [rom_n.get(ch)]
                         else:
                             list_num.append(rom_n.get(ch))
 
                             last_rom = rom_n.get(ch)
 
-                            num+= to_subtract(list_num)
+                            num += to_subtract(list_num)
 
                             return (num)
